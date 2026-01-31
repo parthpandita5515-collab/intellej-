bc9d25325f3f15d6d4828f072506745f98391e4c
parthpandita5515 <parthpandita5515@gmail.com>
2026-01-31 20:31:17 +0530

    Hello World

--- src/main/java/com/Parth/Hello.java
+++ src/main/java/com/Parth/Hello.java
@@ -2,5 +2,10 @@ package com.Parth;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");}
+        Student student = new Student();
+        student.setName("Prestegious Parth Pandita");
+        student.setAge(21);
+        System.out.println(student.getAge());
+        System.out.println(student.getName());
+    }
 }
